4ad6d682d3d7985744099d7d5099394cf84874d5
sixertoy <sixertoy.github@gmail.com>
2019-07-10 05:31:43 +0200

    add dotfiles

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.scss';
+
+class App extends React.PureComponent {
+  render() {
+    return <div>Boilerplate React App</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
